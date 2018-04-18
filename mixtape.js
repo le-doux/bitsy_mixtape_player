3742bbb4ec0c9cb98650fda2a7fc313cb55d3801
Adam Le Doux <adamrossledoux@gmail.com>
2018-04-17 18:07:11 -0700

    mixtape example code

---
+++ mixtape.js
@@ -0,0 +1,299 @@
+var mixtape_games = {
+	castle_island : { 
+		title : "Castle Island", 
+		author : "Breog&#225;n Hackett", 
+		src : "games/castle_island.html", 
+		portfolio : "https://boaheck.itch.io/", 
+		blurb : ""
+	},
+	cats_out_of_the_bag : { 
+		title : "Cat's Out of the Bag!", 
+		author : "Claire Morley",
+		src : "games/cats_out_of_the_bag.html", 
+		portfolio : "http://www.clairemorleyart.com/", 
+		blurb : "I made Cat's Out of the Bag! for the Bitsy Secret jam last year with the idea that it would be heavily inspired by 80s/90s coming of age/high school films like Clueless, Some Kind Of Wonderful, The Breakfast Club etc. I wanted to create lots of interlinking stories that reached across social groups in the school, and tried to give each character a decent story arc, with a secret that Cat could learn and reveal, whether for better or worse. It was pretty challenging fitting all the stories into the game without having too many characters in each scene (I think originally I had double the characters too), and I ended up plotting it all onto a web to see how it connected. It's still the longest Bitsy game I've made by far!"
+	},
+	day_in_the_life : {
+		title : "Day in the Life", 
+		author : "Ethan Clark", 
+		src : "games/day_in_the_life.html", 
+		portfolio : "https://night-driver.itch.io/", 
+		blurb : "At the time I hadn't noticed a great deal of bitsy games that delivered a shock to the player. When the moth jam came about I decided to realize that idea, albeit to what I consider mixed results. Certain elements I had planned didn't work and as a result the finished game is a pared-down version of the story. Instead I used color as the \"gotcha\" to deliver the punchline of the game and it seems to have worked quite well."
+	},
+	human_palace : {
+		title : "going by train to the human palace", 
+		author : "nicole voec", 
+		src : "games/human_palace.html", 
+		portfolio : "https://voec.itch.io/", 
+		blurb : ""
+	},
+	junk_shop_telescope : {
+		title : "Junk Shop Telescope", 
+		author : "Ben Bruce", 
+		src : "games/junk_shop_telescope.html", 
+		portfolio : "https://cephalopodunk.itch.io/", 
+		blurb : "JST started with the (incorrect!) idea that it would be fun to draw the moon in Bitsy. I realized that I could make the 'avatar' a focus point rather than a character, allowing us to zoom in on points of interest - eventually this evolved into being a sort of psychic projection / remote-viewing focal point. I wanted to communicate a Luna mood of pale, cold, and ancient alienation, so I tried telling the story without extended dialogue. Most of my other Bitsy games are very wordy, but there is something very human about communication. Still not convinced that I should have included the three words that I did!"
+	},
+	little_duck : { 
+		title : "Little Duck's Big Adventure", 
+		author : "Em Lindqvist", 
+		src : "games/little_duck.html", 
+		portfolio : "https://empha.itch.io/", 
+		blurb : "Little Duck is an old-school RPG modeled on my favorite games from when I was a kid. It's got nearly everything those games used to have, like shops and spells and dungeons. I don't think this is the most obvious genre for a bitsy game, but almost anything is possible with some hacking. I had a lot of fun challenging myself and figuring out how to make the engine do new things!"
+	},
+	mailfrog : { 
+		title : "mailfrog", 
+		author : "Stuey Stulack", 
+		src : "games/mailfrog.html", 
+		portfolio : "http://www.stueystulack.com/", 
+		blurb : "mailfrog exists as a small world full of animals where nothing else but being kind to one another matters. You play as a frog who delivers mail to his wonderful friends. It was made for a frog themed game jam on http://itch.io.\n\n Having just moved back home after graduating from college, I felt lonely being away from most of my support network and burnt out from working on my thesis for the better part of the past 5 months. mailfrog served as a happy space where I could forget about everything wrong with the world for a short moment in time. It helped me break through a creative block and I hope it helps others forget about their worries too, if for only a short while."
+	},
+	milk_mitten : { 
+		title : "The Milk and Mitten", 
+		author : "Sarah Gould", 
+		src : "games/milk_mitten.html", 
+		portfolio : "https://zenzoa.com/", 
+		blurb : "I made the Milk and Mitten while huddled in a hotel in London, hiding from the bitter cold and biting wind. I just wanted to be someplace warm, cozy, and full of friends. And while a bitsy game cannot provide actual warmth, I know they can make me feel cozy, and I hope this one makes you feel cozy too!"
+	},
+	no_destination : { 
+		title : "no destination", 
+		author : "mark wonnacott", 
+		src : "games/no_destination.html", 
+		portfolio : "https://candle.itch.io/", 
+		blurb : "i made this as a prototype for a part of a game i'd been planning for a while, but i found it much more exciting to try to continue the ideas with follow up bitsy games. after the mountain and tower scenes i got bored of making tiles and made the rest by reusing existing tiles and building ideas around what they ended up looking like"
+	},
+	onion_detective : { 
+		title : "Lil Onion Detective", 
+		author : "onion", 
+		src : "games/onion_detective.html", 
+		portfolio : "https://www.tzvezdina.com/", 
+		blurb : "lil onion detective was just a game i made to prove to myself that i could make something. i didn't really know what i was doing and didn't know how to creatively use all the tools bitsy provides (it took asking a friend to even realize i could use rooms to give players a way to choose the culprit!) i didn't think much of the game as i made it and its still a shock that a lot of people really like it! all i hoped was that it would be an amusing little \"thing\" that a few people might play. i guess all i want to say is don't be afraid to try making games– especially bitsy games!"
+	},
+	slow_growing : { 
+		title : "Slow Growing", 
+		author : "Ebeth", 
+		src : "games/slow_growing.html", 
+		portfolio : "http://www.ebethnorvell.com/", 
+		blurb : "Slow Growing is a about a small snail that wants to have a big garden and its about creating and growing even when you don't always believe in yourself. 🐌🌱"
+	},
+	slug_life : { 
+		title : "Slug Life", 
+		author : "Sekamelica", 
+		src : "games/slug_life.html", 
+		portfolio : "https://sekamelica.itch.io/", 
+		blurb : "I've made Slug Life to talks about consequences of ostracism on the daily lives of queer persons. I also wanted to evoke how important it can be when you're different to find and meet people that are a bit like you, that can understand you, because it really helps.\n\nI wanted to keep my feelings unaltered when making this game, so I've used Bitsy for allowing me to start, finish and release the game in a short period of time, so it was easier to talk about personal stuff."
+	},
+	the_exile : { 
+		title : "The Exile on the Long Shore", 
+		author : "Natalie Clayton", 
+		src : "games/the_exile.html", 
+		portfolio : "https://scarletcatalie.itch.io/", 
+		blurb : "The Exile on the Long Shore began as an exercise in learning How To Bitsy, and turned into a meditative exercise in virtual space and world-building. It featured in Waypoint as \"an antithesis to Open World Clutter\", and this sort of sparse, carefully-curated approach to environments continues to influence Natalie's work."
+	},
+	tower_night : { 
+		title : "The Tower at Night", 
+		author : "G.P. Lackey", 
+		src : "games/tower_night.html", 
+		portfolio : "https://mousefountain.itch.io/", 
+		blurb : ""
+	},
+	worm_dungeon : { 
+		title : "Worm Dungeon", 
+		author : "Daniel Linssen", 
+		src : "games/worm_dungeon.html", 
+		portfolio : "https://managore.itch.io/", 
+		blurb : "I really wanted to make a Bitsy game with weird, friendly characters and as many different puzzles as I could squeeze in. I didn't know much about Bitsy when I started and was curious how far I could stretch it!"
+	},
+	moth : {
+		title : "A Moth Batters Against Glass",
+		author : "Sam Keeper",
+		src : "games/moth.html",
+		portfolio : "https://samkeeper.itch.io",
+		blurb : "I didn't mean to create a Bitsy game. It just seemed so obviously meant for top down pixel art environments, I couldn't restrain myself: I started designing sweeping, vertical staircases instead. The city in A Moth Batters Against Glass was built from the staircases up. \n\nThe main character was built from the \"Moth\" jam down, or rather, he was imported from another game because he was an insect. That was too obvious and literal a connection, though, so the character and his whole civilization developed an unfortunate addiction to pretty lights--like moths to a flame.\n\nBitsy is simple, pared back to only the merest essentials. In its 2d world, lateral moves become all the more interesting."
+	}
+};
+
+function start() {
+	recreateGameList();
+
+	shufflePlayList = makeShuffledGameList();
+	shuffleSelectList = makeShuffledGameList();
+
+	selectRandomGame();
+}
+
+function recreateGameList() {
+	document.getElementById("game_select").innerHTML = "";
+	var shuffledGames = makeShuffledGameList();
+	for(var i in shuffledGames) {
+		var gameId = shuffledGames[i];
+		document.getElementById("game_select").appendChild( makeGameCard(gameId) );
+	}
+}
+
+var shufflePlayList = [];
+var shuffleSelectList = [];
+
+function makeShuffledGameList() {
+	var list = [];
+	for(var gameId in mixtape_games) {
+		list.push(gameId);
+	}
+	list = shuffle(list);
+	return list;
+}
+
+function shuffle(options) {
+	var optionsShuffled = [];
+	var optionsUnshuffled = options.slice();
+	while(optionsUnshuffled.length > 0) {
+		var i = Math.floor( Math.random() * optionsUnshuffled.length );
+		optionsShuffled.push( optionsUnshuffled.splice(i,1)[0] );
+	}
+	return optionsShuffled;
+}
+
+function playRandomGame() {
+	var gameId = shufflePlayList.pop();
+	if(shufflePlayList.length <= 0) {
+		shufflePlayList = makeShuffledGameList();
+	}
+	selectGame(gameId);
+	playGame(gameId);
+}
+
+function selectRandomGame() {
+	var gameId = shuffleSelectList.pop();
+	if(shuffleSelectList.length <= 0) {
+		shuffleSelectList = makeShuffledGameList();
+	}
+	selectGame(gameId);
+}
+
+function makeGameCard(gameId) {
+	var game = mixtape_games[gameId];
+
+	var div = document.createElement("div");
+	div.onclick = function() {
+		if(selectedGameId != gameId) {
+			selectGame(gameId,slowTimer);
+		}
+		else {
+			playGame(gameId);
+		}
+	};
+	div.id = "card_" + gameId;
+	div.classList.add("game_card");
+
+	var gif = document.createElement("img");
+	gif.src = "images/" + gameId + ".gif";
+	gif.width = 150;
+	gif.height = 150;
+	div.appendChild(gif);
+
+	var title = document.createElement("h4");
+	title.innerText = game.title;
+	div.appendChild(title);
+
+	// var author = document.createElement("h5");
+	// author.innerText = game.author;
+	// div.appendChild(author);
+
+	return div;
+}
+
+var selectedGameId = null;
+var nextGameTimer = null;
+var fastTimer = 4000;
+var slowTimer = 60000;
+
+function selectGame(gameId,timer) {
+	if(timer == undefined || timer == null)
+		timer = fastTimer;
+
+	console.log(selectedGameId);
+
+	if(selectedGameId != null){
+		document.getElementById("card_" + selectedGameId).classList.remove("selected_card");
+		document.getElementById("select_screen").classList.remove(selectedGameId);
+	}
+
+	selectedGameId = gameId;
+
+	var game = mixtape_games[gameId];
+
+	document.getElementById("card_" + gameId).classList.add("selected_card");
+
+	document.getElementById("game_info_title").innerText = game.title;
+	document.getElementById("game_info_author").innerHTML = game.author;
+	document.getElementById("game_info_img").src = "images/" + gameId + ".gif";
+	document.getElementById("game_info_blurb").innerText = game.blurb;
+
+	document.getElementById("game_info_portfolio_link").href = game.portfolio;
+	document.getElementById("game_info_portfolio_author").innerHTML = game.author;
+
+	document.getElementById("game_info_title").onclick = function() {
+		playGame(gameId);
+	};
+	document.getElementById("game_info_img").onclick = function() {
+		playGame(gameId);
+	};
+	document.getElementById("game_info_play_button").onclick = function() {
+		playGame(gameId);
+	};
+
+	document.getElementById("select_screen").classList.add(selectedGameId);
+
+	if(nextGameTimer != null)
+		window.clearTimeout(nextGameTimer);
+	nextGameTimer = window.setTimeout(selectRandomGame, timer);
+}
+
+function playGame(gameId) {
+	var game = mixtape_games[gameId];
+
+	if(nextGameTimer != null)
+		window.clearTimeout(nextGameTimer);
+	selectedGameId = gameId;
+
+	// remove current game from randomization list
+	if(shufflePlayList.indexOf(gameId) != -1) {
+		shufflePlayList.splice(shufflePlayList.indexOf(gameId),1);
+	}
+	// regenerate randomization list if necessary
+	if(shufflePlayList.length <= 0) {
+		shufflePlayList = makeShuffledGameList();
+	}
+
+	document.getElementById("select_screen").style.display = "none";
+	document.getElementById("play_screen").style.display = "block";
+
+	document.getElementById("play_title").innerText = game.title;
+	document.getElementById("play_author").innerHTML = game.author;
+
+	document.getElementById("play_exit_button").onclick = exitPlayScreen;
+	document.getElementById("play_restart_button").onclick = restartGame;
+	document.getElementById("play_random_button").onclick = playRandomGame;
+
+	var frame = document.getElementById("game_frame");
+	frame.src = "";
+
+	window.setTimeout(function(){
+		frame.src = game.src;
+		frame.focus();
+	}, 200);
+}
+
+function exitPlayScreen() {
+	document.getElementById("game_frame").src = "";
+	document.getElementById("select_screen").style.display = "block";
+	document.getElementById("play_screen").style.display = "none";
+
+	recreateGameList();
+
+	selectGame(selectedGameId);
+}
+
+function restartGame() {
+	playGame(selectedGameId);
+}
